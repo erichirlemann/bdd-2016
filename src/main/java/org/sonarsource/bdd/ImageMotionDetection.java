d4f627c0451063100ec3f0fb38d98511cb8f2f19
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-10-07 10:09:28 +0200

    Convert diff image to gray

--- src/main/java/org/sonarsource/bdd/ImageMotionDetection.java
+++ src/main/java/org/sonarsource/bdd/ImageMotionDetection.java
@@ -4,13 +4,17 @@ import java.io.File;
 import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.bytedeco.javacv.FrameGrabber;
 
+import static org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U;
 import static org.bytedeco.javacpp.opencv_core.cvAbsDiff;
+import static org.bytedeco.javacpp.opencv_core.cvCreateImage;
 import static org.bytedeco.javacpp.opencv_core.cvGetSize;
 import static org.bytedeco.javacpp.opencv_core.cvReleaseImage;
 import static org.bytedeco.javacpp.opencv_imgcodecs.cvLoadImage;
 import static org.bytedeco.javacpp.opencv_imgcodecs.cvSaveImage;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_BLUR;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_RGB2GRAY;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_THRESH_BINARY;
+import static org.bytedeco.javacpp.opencv_imgproc.cvCvtColor;
 import static org.bytedeco.javacpp.opencv_imgproc.cvSmooth;
 import static org.bytedeco.javacpp.opencv_imgproc.cvThreshold;
 
@@ -30,22 +34,28 @@ public class ImageMotionDetection {
 
     IplImage diff = diff(image1, image2);
     // do some threshold for wipe away useless details
-    cvThreshold(diff, diff, 110, 255, CV_THRESH_BINARY);
+    cvThreshold(diff, diff, 130, 255, CV_THRESH_BINARY);
     cvSmooth(diff, diff, CV_BLUR, 9, 9, 2, 2);
+    IplImage copy = cvCreateImage(cvGetSize(diff), IPL_DEPTH_8U, 1);
+    cvCvtColor(diff, copy, CV_RGB2GRAY);
 
-    cvSaveImage("target/diff.JPG", diff);
-    cvReleaseImage(diff);
+    cvSaveImage("target/diff.JPG", copy);
+    cvReleaseImage(copy);
     cvReleaseImage(image1);
     cvReleaseImage(image2);
   }
 
   private IplImage diff(IplImage frame, IplImage frame2) {
-    IplImage diff = IplImage.create(cvGetSize(frame), frame.depth(), frame.nChannels());
+    IplImage diff = copy(frame);
 
-    //cvSmooth(frame, frame, CV_GAUSSIAN, 9, 9, 2, 2);
+    // cvSmooth(frame, frame, CV_GAUSSIAN, 9, 9, 2, 2);
     // perform ABS difference
     cvAbsDiff(frame, frame2, diff);
     return diff;
   }
 
+  private static IplImage copy(IplImage frame) {
+    //return frame.clone();
+    return IplImage.create(cvGetSize(frame), frame.depth(), frame.nChannels());
+  }
 }
