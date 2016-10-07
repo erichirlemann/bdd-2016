36a15106c4ec5e63f8f5e0dd7f94dc53231a4557
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-10-07 08:00:08 +0200

    replace opencv by javacv

--- src/main/java/org/sonarsource/bdd/App.java
+++ src/main/java/org/sonarsource/bdd/App.java
@@ -1,14 +1,11 @@
 package org.sonarsource.bdd;
 
-import org.opencv.core.Core;
-
 /**
  * Hello world!
  *
  */
 public class App {
   public static void main(String[] args) {
-    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     new BackgroundSubtraction().execute();
   }
 }
