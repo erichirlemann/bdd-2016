4aeb3b2af14d8d4acf8e2c0654568468500246b9
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-10-07 11:43:57 +0200

    use cvFindContours

--- src/main/java/org/sonarsource/bdd/ImageMotionDetection.java
+++ src/main/java/org/sonarsource/bdd/ImageMotionDetection.java
@@ -1,10 +1,12 @@
 package org.sonarsource.bdd;
 
 import java.io.File;
+import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacpp.opencv_core;
 import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.bytedeco.javacv.FrameGrabber;
 
+import static org.bytedeco.javacpp.helper.opencv_imgproc.cvFindContours;
 import static org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U;
 import static org.bytedeco.javacpp.opencv_core.cvAbsDiff;
 import static org.bytedeco.javacpp.opencv_core.cvAnd;
@@ -16,16 +18,19 @@ import static org.bytedeco.javacpp.opencv_core.cvScalar;
 import static org.bytedeco.javacpp.opencv_imgcodecs.cvLoadImage;
 import static org.bytedeco.javacpp.opencv_imgcodecs.cvSaveImage;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_BLUR;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_CHAIN_APPROX_SIMPLE;
+import static org.bytedeco.javacpp.opencv_imgproc.CV_RETR_TREE;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_RGB2GRAY;
 import static org.bytedeco.javacpp.opencv_imgproc.CV_THRESH_BINARY;
 import static org.bytedeco.javacpp.opencv_imgproc.cvCvtColor;
+import static org.bytedeco.javacpp.opencv_imgproc.cvMinAreaRect2;
 import static org.bytedeco.javacpp.opencv_imgproc.cvSmooth;
 import static org.bytedeco.javacpp.opencv_imgproc.cvThreshold;
 
 public class ImageMotionDetection {
 
-  //  static opencv_core.CvScalar min = cvScalar(0x1c, 0x89, 0xb5, 0);// BGR-A
-  //  static opencv_core.CvScalar max = cvScalar(0x3b, 0xaf, 0xe4, 0);// BGR-A
+  // static opencv_core.CvScalar min = cvScalar(181, 137, 28, 0);// BGR-A
+  // static opencv_core.CvScalar max = cvScalar(228, 175, 59, 0);// BGR-A
 
   static opencv_core.CvScalar min = cvScalar(0x09, 0x63, 0x90, 0);// BGR-A
   static opencv_core.CvScalar max = cvScalar(0x2c, 0xB3, 0xe4, 0);// BGR-A
@@ -57,8 +62,9 @@ public class ImageMotionDetection {
     IplImage result = cvCreateImage(cvGetSize(image1), IPL_DEPTH_8U, 1);
     cvInRangeS(copyAnd, min, max, result);
 
-    cvSaveImage("target/inter.JPG", copyAnd);
-    cvSaveImage("target/diff.JPG", result);
+    contours(result);
+
+    save(result, "final");
     cvReleaseImage(copyAnd);
     cvReleaseImage(image1);
     cvReleaseImage(image2);
@@ -71,6 +77,34 @@ public class ImageMotionDetection {
     // perform ABS difference
     cvAbsDiff(frame, frame2, diff);
     return diff;
+  }
+
+  private void contours(IplImage image) {
+    IplImage contourImage = image.clone();
+    cvSmooth(contourImage, contourImage, CV_BLUR, 9, 9, 2, 2);
+
+    opencv_core.CvMemStorage storage = opencv_core.CvMemStorage.create();
+    opencv_core.CvSeq contour = new opencv_core.CvSeq(null);
+    cvFindContours(contourImage, storage, contour, Loader.sizeof(opencv_core.CvContour.class), CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
+    System.out.println("Contours : " + contour.total());
+
+    while (contour != null && !contour.isNull()) {
+      if (contour.elem_size() > 0) {
+        opencv_core.CvBox2D box = cvMinAreaRect2(contour, storage);
+        // test intersection
+        if (box != null) {
+          opencv_core.CvPoint2D32f center = box.center();
+          opencv_core.CvSize2D32f size = box.size();
+          System.out.println("sizes : " + size);
+        }
+      }
+      contour = contour.h_next();
+    }
+    save(contourImage, "contour");
+  }
+
+  private static void save(IplImage frame, String name) {
+    cvSaveImage(String.format("target/%s.JPG", name), frame);
   }
 
   private static IplImage copy(IplImage frame) {
